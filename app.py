7d33f6c5421e163058e5075018f2491a5d199bbf
kashishsehgal73 <kashishsehgal73@gmail.com>
2019-06-06 13:01:46 +0530

    fir

--- app.py
+++ app.py
@@ -5,24 +5,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+	return "Hello, World!"
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Fetch the message
-    msg = request.form.get('Body')
+	"""Respond to incoming calls with a simple text message."""
+	# Fetch the message
+	msg = request.form.get('Body')
 
 	resp = detect_intent_from_text(str(msg),999)
 
-    return str(resp.fulfillment_text)
+	return str(resp.fulfillment_text)
 
 def detect_intent_from_text(text, session_id, language_code='en'):
-    session = dialogflow_session_client.session_path(PROJECT_ID, session_id)
-    text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
-    query_input = dialogflow.types.QueryInput(text=text_input)
-    response = dialogflow_session_client.detect_intent(session=session, query_input=query_input)
-    return response.query_result
+	session = dialogflow_session_client.session_path(PROJECT_ID, session_id)
+	text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
+	query_input = dialogflow.types.QueryInput(text=text_input)
+	response = dialogflow_session_client.detect_intent(session=session, query_input=query_input)
+	return response.query_result
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
