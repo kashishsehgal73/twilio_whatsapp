5277754c5aa2e9d021746df415c4f191ed6cb751
kashishsehgal73 <kashishsehgal73@gmail.com>
2019-06-08 21:19:14 +0530

    FW

--- cric_api.py
+++ cric_api.py
@@ -7,7 +7,8 @@ class kashish_assistant():
 
 	def crawl_url(self,url):
 		try:
-			r = requests.get(url)
+			headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+			r = requests.get(url, headers= headers )
 			print(r, r.text)
 			return r.json()
 		except Exception:
