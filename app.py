3f565f071c7fa799331caedb2c8f736ebe961d98
kashishsehgal73 <kashishsehgal73@gmail.com>
2019-06-09 12:50:47 +0530

    weather

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import os
-from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.messaging_response import Body, Media, Message, MessagingResponse
+import requests
 from utils import fetch_reply
 
 import dialogflow_v2 as dialogflow
@@ -14,16 +15,30 @@ def hello():
 	return "Hello, World!"
 
 @app.route("/sms", methods=['POST'])
-def sms_reply():
 
 	msg = request.form.get('Body')
 	sender = request.form.get('From')
-	resp = fetch_reply(msg,sender)
-	response = MessagingResponse()
-	response.message(resp)
-	return str(response)
+    # Create reply
+    resp = MessagingResponse()
+    recieved_obj = fetch_reply(msg,sender)
 
+    # If the recieved object is from get_weather()
+    if isinstance(recieved_obj[0], str):
+        message = Message()
+        message.body(recieved_obj[0])
+        message.media(recieved_obj[1])
+        resp.append(message)
+        return str(resp)
 
+    # If the recieved object is from get_news()
+    for row in recieved_obj:
+        message = Message()
+        link = requests.get('http://tinyurl.com/api-create.php?url={}'.format(row['link'])).content.decode('utf-8')
+        message.body("{}\n{}".format(row['title'],link))
+        if row['media'] :
+            message.media(row['media'])
+        resp.append(message)
+return str(resp)
 
 if __name__ == "__main__":
 	app.run(use_reloader=True)
