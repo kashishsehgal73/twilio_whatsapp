4ec9db2d94d527e742907ac29ad7f3c62ed9d676
kashishsehgal73 <kashishsehgal73@gmail.com>
2019-06-09 13:54:15 +0530

    weather123

--- app.py
+++ app.py
@@ -18,8 +18,28 @@ def hello():
 def sms_reply():
 	msg = request.form.get('Body')
 	sender = request.form.get('From')
-	return fetch_reply(msg,sender)
+	# Create reply
+	resp = MessagingResponse()
+	recieved_obj = fetch_reply(msg,sender)
 
+	# If the recieved object is from get_weather or default responses from dialogflow
+	if isinstance(recieved_obj[0], str):
+		message = Message()
+		message.body(recieved_obj[0])
+		if recieved_obj[i] :
+			message.media(recieved_obj[1])
+		resp.append(message)
+		return str(resp)
+
+	# If the recieved object is from get_news()
+	for row in recieved_obj:
+		message = Message()
+		link = requests.get('http://tinyurl.com/api-create.php?url={}'.format(row['link'])).content.decode('utf-8')
+		message.body("{}\n{}".format(row['title'],link))
+		if row['media'] :
+			message.media(row['media'])
+		resp.append(message)
+	return str(resp)
 
 if __name__ == "__main__":
 	app.run(use_reloader=True)
--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
 from cricket import get_score,list_matches
 from weather_news import get_news,get_weather
 import dialogflow_v2 as dialogflow
-from twilio.twiml.messaging_response import Body, Media, Message, MessagingResponse
 
-import os
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "client-secret.json"
 dialogflow_session_client = dialogflow.SessionsClient()
 PROJECT_ID = "ks73wbot-kwqfwu"
+
+
 
 def detect_intent_from_text(text, session_id, language_code='en'):
     session = dialogflow_session_client.session_path(PROJECT_ID, session_id)
@@ -17,46 +17,16 @@ def detect_intent_from_text(text, session_id, language_code='en'):
     return response.query_result
 
 def fetch_reply(msg, session_id):
-
-	resp = MessagingResponse()
 	response = detect_intent_from_text(msg, session_id)
-
+	print(dict(response.parameters))
 	if response.intent.display_name == 'get_news':
 		value =  get_news(dict(response.parameters),session_id)
-
-		for row in value:
-			message = Message()
-			link = requests.get('http://tinyurl.com/api-create.php?url={}'.format(row['link'])).content.decode('utf-8')
-			message.body("{}\n{}".format(row['title'],link))
-			if row['media'] :
-				message.media(row['media'])
-			resp.append(message)
-		return resp
-
-
-
 	elif response.intent.display_name == 'get_weather':
-		value =  get_weather(dict(response.parameters),session_id)
-		message = Message()
-		message.body(value[0])
-		message.media(value[1])
-		resp.append(message)
-		return resp
-
-
-
+		return get_weather(dict(response.parameters),session_id)
 	elif response.intent.display_name == 'cricket_score':
 		value =  get_score(dict(response.parameters))
-		return resp.message(str(value))
-
-
-
 	elif response.intent.display_name == 'match_list':
 		value = list_matches(dict(response.parameters))
-		return resp.message(str(value))
-
-
 	else:
-
-		value = response.fulfillment_text
-		return resp.message(str(value))
+		value = response.fulfillment_text, False
+	return value
