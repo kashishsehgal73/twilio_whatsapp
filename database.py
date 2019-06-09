c12337fdb0f6719c36d74319f9208fdf1d636d52
kashishsehgal73 <kashishsehgal73@gmail.com>
2019-06-09 12:59:24 +0530

    weather12

--- database.py
+++ database.py
@@ -1,17 +1,17 @@
 from pymongo import MongoClient
 client = MongoClient("mongodb+srv://kashish:chayansehgal3@cluster0-763oj.mongodb.net/test?retryWrites=true&w=majority")
 database = client.get_database('cricket_db')
-db = database.messages
+records = database.messages
 
 
 
 def update_records(session_id, top, loc, cit):
 	if top:
-		db.people.update_one({'from': session_id}, {'$set': {'news_type': top }}, upsert=True)
+		records.update_one({'from': session_id}, {'$set': {'news_type': top }}, upsert=True)
 	if loc:
-		db.people.update_one({'from': session_id}, {'$set': {'geo-country': loc }}, upsert=True)
+		records.update_one({'from': session_id}, {'$set': {'geo-country': loc }}, upsert=True)
 	if cit:
-		db.people.update_one({'from': session_id}, {'$set': {'geo-city': cit }}, upsert=True)
+		records.update_one({'from': session_id}, {'$set': {'geo-city': cit }}, upsert=True)
 	a = records.find({'from': session_id})[0]
 	return a['news_type'], a['geo-country'], a['geo-city']
 
